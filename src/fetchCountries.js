c1c5393a764a8da42f93802e62c2095fa6e23876
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-02-28 14:47:08 +0200

    add all functions

--- src/fetchCountries.js
+++
@@ -1,7 +0,0 @@
-export const fetchCountries = name => {
-  return fetch(
-    `https://restcountries.com/v2/name/${name.currentTarget.value}?fields=name,capital,population,languages.name,flags`
-  ).then(response => {
-    return response.json();
-  });
-};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -10,41 +10,74 @@ const refs = {
   countryList: document.querySelector('.country-list'),
   countryinfo: document.querySelector('.country-info'),
 };
+let seachCountry = '';
 
-refs.input.addEventListener('input', e => {
-  fetchCountries(e).then(countriesList => {
-    console.log(countriesList);
-    if (countriesList.length === 1) {
-      refs.countryinfo.innerHTML = createCountryCard(countriesList[0]);
-      refs.countryList.innerHTML = '';
-      return;
-    }
-    if (countriesList.length > 1 && countriesList.length < 10) {
-      refs.countryList.innerHTML = createCountriesList(countriesList);
-      refs.countryinfo.innerHTML = '';
-      return;
-    } else return;
-  });
-});
+const creatPage = () => {
+  seachCountry = refs.input.value.trim();
+  if (seachCountry != '') {
+    fetchCountries(seachCountry)
+      .then(countriesList => {
+        if (countriesList.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          cleanList();
+        } else if (countriesList.length === 1) {
+          refs.countryinfo.innerHTML = createCountryCard(countriesList[0]);
+          refs.countryList.innerHTML = '';
+          return;
+        } else if (countriesList.length > 1 && countriesList.length < 10) {
+          refs.countryList.innerHTML = createCountriesList(countriesList);
+          refs.countryinfo.innerHTML = '';
+          return;
+        } else {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+          cleanList();
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else {
+    cleanList();
+    return;
+  }
+};
+
+refs.input.addEventListener('input', debounce(creatPage, DEBOUNCE_DELAY));
 
 const createCountriesList = countriesList => {
   return countriesList
     .map(country => {
       return `<li class="country-item">
-<img class="country-flag" src="${country.flags.svg}" alt="${country.name} flag">
-<p class="country-name">${country.name}</p>
-</li>`;
+                <img class="country-flag" src="${country.flags.svg}" alt="flag of ${country.name} ">
+                <p class="country-name">${country.name}</p>
+              </li>`;
     })
     .join('');
 };
 
 const createCountryCard = country => {
+  const languages = languageList(country.languages);
   const card = `<h2 class="card-name">
-      <img class="country-flag" src="${country.flags.svg}" alt="${country.name} flag"> ${country.name}
-    </h2>
-    <p class="cardText">Capital: <span class="card-info">${country.capital}</span></p>
-    <p class="cardText">Population: <span class="card-info">${country.population}</span></p>
-    <p class="cardText">Languages: <span class="card-info">${country.languages}</span></p>`;
+                  <img class="country-flag" src="${country.flags.svg}" alt="flag of ${country.name} "> ${country.name}
+                </h2>
+                <p class="card-text">Capital: <span class="card-info">${country.capital}</span></p>
+                <p class="card-text">Population: <span class="card-info">${country.population}</span></p>
+                <p class="card-text">Languages: <span class="card-info">${languages}</span></p>`;
 
   return card;
+};
+
+const languageList = languages => {
+  return languages
+    .map(language => {
+      return language.name;
+    })
+    .join(', ');
+};
+
+const cleanList = () => {
+  refs.countryinfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
 };
